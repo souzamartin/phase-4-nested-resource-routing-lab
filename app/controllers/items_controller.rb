569081ff979a9e8bf14219fa03f7a1d7ef3efa5d
Martin <souzam@sonic.net>
2022-12-15 11:39:40 -0500

    commit tree 4eab23fa0e1af4899edb64ccbac7c0cd5c75f915

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,31 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
 
   def index
-    items = Item.all
-    render json: items, include: :user
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      render json: user.items, status: :ok
+    else
+      render json: Item.all, include: :user, status: :ok
+    end
   end
 
+  def show
+    render json: Item.find(params[:id]), status: :ok
+  end
+
+  def create
+    user = User.find(params[:user_id])
+    item = user.items.create!(item_params)
+    render json: item, status: :created
+  end
+
+  private
+  def item_params
+    params.permit(:name, :description, :price)
+  end
+
+  def render_not_found
+    render json: {error: "Record not found"}, status: :not_found
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,13 @@
 class UsersController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
 
   def show
-    user = User.find_by(id: params[:id])
+    user = User.find(params[:id])
     render json: user, include: :items
   end
 
+  private
+  def render_not_found
+    render json: {error: "Record not found"}, status: :not_found
+  end
 end
